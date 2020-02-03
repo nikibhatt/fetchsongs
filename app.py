2c27e8ee7f2bbe404c4b011b9fb16e422ef48c2b
nikibhatt <nkibhatt@users.noreply.github.com>
2020-02-03 12:33:53 -0800

    still simple app

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
-from .models import *
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
+import requests
 
 APP = Flask(__name__)
+FLASK_DEBUG=1
 # server name is specified in proc file
 APP.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///MedCabinet.db"
-DB.init_app(app)
+#DB.init_app(app)
 
 def predict(input_song):
-    return input_song.to_json(orient='columns')
+    return input_song
 
 @APP.route('/', methods=['GET', 'POST'])
 def root():
-    return "Spotify song selector API"
+    song_id = ''
+    if request.method == "POST":
+        song_id = request.form['song_id']
+        output = predict(song_id)
+    return render_template('base.html', song_id=output)
 
-@APP.route('/song', method=['POST'])
+@APP.route('/song', methods=['GET', 'POST'])
 def song():
-    song_selected = request.get_json(force=True)
-    song_id = song_selected['input']
-    assert isinstance(song_id, str)
-    output = predict(song_id)
-    return output
+    return render_template('index.html')
 
-if __name == "__main":
+if __name__ == "__main__":
     APP.run()
